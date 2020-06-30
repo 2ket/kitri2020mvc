1d0eaf9caa3d29aac6d22d2bde57b64ce69ada29
2ket <hello2ket@gmail.com>
2020-06-30 11:25:37 +0900

    interface함수를 overriding하여 각 Action클래스들 한번에사용, init()에 HashMap<key,Value>

--- mvcExample/src/com/java/mvc04/CommandCnt.java
+++ mvcExample/src/com/java/mvc04/CommandCnt.java
@@ -1,6 +1,7 @@
 package com.java.mvc04;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -15,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 //@WebServlet("/CommandCnt") 어노테이션 나중에!
 public class CommandCnt extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    private HashMap<String, Object> commandMap=new HashMap<String, Object>();
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -24,22 +25,42 @@ public class CommandCnt extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
+    @Override
+    public void init() throws ServletException {
+    	//if(cmd.equals("/write.act")) {
+		//	commandAction=new WriteAction();
+    	//이부분 계속 반복해서 쓰는게 코드낭비이므로 init에 Map으로 지정해줘서 자동으로 연결되게 할거에요
+    	//맨위에 전역에         private HashMap<String, Object> commandMap=new HashMap<String, Object>();
+    	
+    	
+    	WriteAction write=new WriteAction();
+    	commandMap.put("/write.act", write);
+    	
+    	ListAction list=new ListAction();
+    	commandMap.put("/list.act", list);
+    }
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String cmd=request.getRequestURI();
 		cmd=cmd.substring(request.getContextPath().length());
-		System.out.println(cmd);
+		System.out.println(cmd);	//HashMap의 key값
+
 		
-		String view=null;
-		CommandAction commandAction=null;
-		if(cmd.equals("/write.act")) {
-			commandAction=new WriteAction();
-		}else if(cmd.equals("/list.act")) {
-			commandAction=new ListAction();
-		}
-		view=commandAction.actionDo(request, response);	//Interface를 사용하여 한번에 view설정
+		CommandAction commandAction=(CommandAction) commandMap.get(cmd);
+		//HashMap commandMap의 값이 Object이므로 Object를 상속받는 Interface CommandAction으로 받으면 공통적으로 쓸 수 있다.
+		
+		//	HashMap commandMap 을 이용하면 이렇게 매번 if로 설정 안해도 된다.
+//		String view=null;
+//		CommandAction commandAction=null;
+//		if(cmd.equals("/write.act")) {
+//			commandAction=new WriteAction();
+//		}else if(cmd.equals("/list.act")) {
+//			commandAction=new ListAction();
+//		}	
+		
+		String view=commandAction.actionDo(request, response);	//Interface를 사용하여 매 클래스들의 overriding한 함수를 한번에 view설정
 		if(view!=null) {
 			RequestDispatcher rd=request.getRequestDispatcher(view);
 			rd.forward(request, response);
