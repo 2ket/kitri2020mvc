bb20350d789035651051f4350edfe6ccbeb15db7
2ket <hello2ket@gmail.com>
2020-07-09 15:43:24 +0900

    fileBoard 관련 기존 package 복사

---
+++ MVCHomepage/src/com/java/fileBoard/command/UpdateOkCommand.java
@@ -0,0 +1,32 @@
+package com.java.fileBoard.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.java.board.model.BoardDao;
+import com.java.board.model.BoardDto;
+import com.java.command.Command;
+
+public class UpdateOkCommand implements Command {
+
+	@Override
+	public String proRequest(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		request.setCharacterEncoding("utf-8");
+		
+		BoardDto boardDto=new BoardDto();
+		
+		boardDto.setBoardNumber(Integer.parseInt(request.getParameter("boardNumber")));
+		boardDto.setEmail(request.getParameter("email"));
+		boardDto.setSubject(request.getParameter("subject"));
+		boardDto.setContent(request.getParameter("content"));
+		
+		int check=BoardDao.getInstance().update(boardDto);
+		logger.info(logMsg+"수정결과: "+check);
+		
+		request.setAttribute("pageNumber", request.getParameter("pageNumber"));
+		request.setAttribute("boardNumber", request.getParameter("boardNumber"));
+		request.setAttribute("check", check);
+		return "/WEB-INF/views/board/updateOk.jsp";
+	}
+
+}
---
+++ MVCHomepage/src/com/java/fileBoard/command/WriteCommand.java
@@ -0,0 +1,39 @@
+package com.java.fileBoard.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.java.command.Command;
+
+public class WriteCommand implements Command {
+
+	@Override
+	public String proRequest(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		//부모글일 때*(ROOT)
+		int boardNumber=0;		// ROOT글이면 0, 
+		int groupNumber=1;		// 그룹번호
+		int sequenceNumber=0;	// 글 순서
+		int sequenceLevel=0;	// 글 레벨
+		
+		//답글일때 boardNumber=> ROOT의 글번호, 그룹번호, 글순서, 글레벨
+		if(request.getParameter("boardNumber")!=null) {
+			//나중에
+			boardNumber=Integer.parseInt(request.getParameter("boardNumber"));
+			groupNumber=Integer.parseInt(request.getParameter("groupNumber"));
+			sequenceNumber=Integer.parseInt(request.getParameter("sequenceNumber"));
+			sequenceLevel=Integer.parseInt(request.getParameter("sequenceLevel"));
+		}
+		
+		
+		
+		logger.info(logMsg+boardNumber+"\t"+groupNumber+"\t"+sequenceNumber+"\t"+sequenceLevel);
+		
+		request.setAttribute("boardNumber", boardNumber);
+		request.setAttribute("groupNumber", groupNumber);
+		request.setAttribute("sequenceNumber", sequenceNumber);
+		request.setAttribute("sequenceLevel", sequenceLevel);
+		
+		return "/WEB-INF/views/board/write.jsp";
+	}
+
+}
