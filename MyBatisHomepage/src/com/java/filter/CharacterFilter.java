20f584cfea8e44ced2dcf7dcb3b756549f5335bd
2ket <hello2ket@gmail.com>
2020-07-13 09:33:10 +0900

    한글필터 chain.doFilter(request, response)

--- MyBatisHomepage/src/com/java/filter/CharacterFilter.java
+++ MyBatisHomepage/src/com/java/filter/CharacterFilter.java
@@ -14,7 +14,10 @@ public class CharacterFilter implements Filter{
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		// TODO Auto-generated method stub
-		
+		request.setCharacterEncoding("utf-8");
+		chain.doFilter(request, response);
+		//요청이 들어오는 작업은 다 필터 작업을 거쳐서 넘어가게 됨
+		//web.xml에서 /*로 슬러시가 그어진 모든 주소에 적용됨. 우선순위) DBinit-CharFilter(/*)-Controller(*.do)
 	}
 
 }
